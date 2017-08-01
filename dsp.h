78aae354d608dd43fc77719bdad1f24614c5934a
duyipai <duyipai@sjtu.edu.cn>
2017-08-01 17:05:06 +0800

    finished dsp

--- dsp.h
+++ dsp.h
@@ -7,5 +7,13 @@ struct status
     unsigned char statusC=0;
 };
 
+struct Receiver
+{
+    unsigned char arrA[30];
+    unsigned char arrB[30];
+    unsigned char arrC[30];
+} receiver;
+
+void pushStatus();
 status getNote();
 #endif // 
