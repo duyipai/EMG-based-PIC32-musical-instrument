e52f6014dda2b5c942b195f3f8a3450c62cc8a97
CarpdiemLiang <liangtao@sjtu.edu.cn>
2017-08-03 17:42:22 +0800

    liang

--- acquisition.c
+++ acquisition.c
@@ -1,29 +1,29 @@
 #include <p32xxxx.h>
-#include <stdio.h>
+
 #include <plib.h>
-#include <stdlib.h>
+
 #include <string.h>
 #include <kmem.h>
+//#include <dsp.h>
 #pragma interrupt ADC_interrupt ipl2 vector 27
-#pragma interrupt T2_ISR ipl3 vector 8
 #pragma interrupt DMA0_ISR ipl1 vector 36
 #pragma interrupt DMA1_ISR ipl1 vector 37
 #pragma interrupt DMA2_ISR ipl1 vector 38
 
-int flags=0;
-int buff = 0;
-unsigned char valueA,valueB,valueC,nonidea;
-unsigned char arrA1[30];
-unsigned char destA[30];
-unsigned char arrA2[30];
-unsigned char arrB1[30];
-unsigned char destB[30];
-unsigned char arrB2[30];
-unsigned char arrC1[30];
-unsigned char destC[30];
-unsigned char arrC2[30];
+static int flags=0;
+static int buff = 0;
+static unsigned char valueA,valueB,valueC,nonidea;
+static unsigned char arrA1[30];
+static unsigned char destA[30];
+static unsigned char arrA2[30];
+static unsigned char arrB1[30];
+static unsigned char destB[30];
+static unsigned char arrB2[30];
+static unsigned char arrC1[30];
+static unsigned char destC[30];
+static unsigned char arrC2[30];
 
-int count=0;
+static int count=0;
 
 void MCU_init() {
 
@@ -31,13 +31,6 @@ void MCU_init() {
 /* setup Timer to count for 1 us and 1 ms */
 INTCONbits.MVEC = 1; // Enable multiple vector interrupt
 asm ("ei"); // Enable all interrupts
-IPC2SET = 0x0000000c; // Interrupt priority level 3, Subpriority level 0
-IFS0CLR = 0x00000100; // Clear timer interrupt flag
-IEC0SET = 0x00000100; // Enable Timer3 interrupt
-T2CON = 0x0; // Stop any 16/32-bit Timer2 operation
-T2CONSET =  0x0020; // Enable 16-bit mode, prescaler 1:4,
-                    // internal clock
-TMR2 = 0x0; // Clear contents of TMR2
 
 } 
 
@@ -48,49 +41,6 @@ TMR2 = 0x0; // Clear contents of TMR2
 
 
 
-/* configure timer SFRs to generate num us delay*/
-void DelayUsec(int num) {
-PR2 = 2*num; // Load PR2
-TMR2 = 0x0; // Clear contents of TMR2
-T2CONSET = 0x8000; // Start Timer 2
-while(1)
-{
-if(flags==1)
-{
-	flags=0;
-	break;
-}
-}
-T2CONCLR = 0x8000; // Close Timer 2
-}
-/* configure timer SFRs to generate 1 ms delay*/
-void GenMsec() {
-PR2 = 2000; // Load PR2
-TMR2 = 0x0; // Clear contents of TMR2
-T2CONSET = 0x8000; // Start Timer 2
-while(1)
-{
-if(flags==1)
-{
-	flags=0;
-	break;
-}
-}
-T2CONCLR = 0x8000; // Close Timer 2
-}
-/* Call GenMsec() num times to generate num ms delay*/
-void DelayMsec(int num) {
-unsigned char i;
-for (i=0; i<num; i++) {
-GenMsec();
-}
-}
-/* timer interrupt handler */
-void T2_ISR (void) 
-{
-flags=1;
-IFS0CLR = 0x0100; // Clear timer 2 interrupt flag
-}
 
 
 void ADCcSonfig(void){
@@ -178,10 +128,7 @@ nonidea=ADC1BUF2;}
     arrC1[count]=valueC;
     count++;
 
-/*	IEC0SET = 0x00000100; // Enable Timer3 interrupt
-	LCD_puts(startStr1);
-	LCD_goto(0x00);
-	IEC0CLR = 0x00000100; // Disable Timer3 interrupt*/
+
 	
 	IFS1CLR = 0x2; 		// Clear ADC interrupt flag
 
@@ -200,27 +147,27 @@ DCH0ECON=0; // no start or stop IRQ, no pattern match
 DCH1ECON=0; // no start or stop IRQ, no pattern match
 DCH2ECON=0; // no start or stop IRQ, no pattern match
 /********* program the transfer *********/
-DCH0SSA=KVA_TO_PA(&arrA2); // transfer source physical address
-DCH0DSA=KVA_TO_PA(&destA); // transfer destination physical address
+DCH0SSA=KVA_TO_PA(&arrA2[0]); // transfer source physical address
+DCH0DSA=KVA_TO_PA(&destA[0]); // transfer destination physical address
 DCH0SSIZ=30; // source size 30 bytes
 DCH0DSIZ=30; // destination size 30 bytes
 DCH0CSIZ=30; // 30 bytes transferred per event
-DCH1SSA=&arrB1+0x40000000; // transfer source physical address
-DCH1DSA=&destB+0x40000000; // transfer destination physical address
+DCH1SSA=KVA_TO_PA(&arrB2[0]); // transfer source physical address
+DCH1DSA=KVA_TO_PA(&destB[0]); // transfer destination physical address
 DCH1SSIZ=30; // source size 30 bytes
 DCH1DSIZ=30; // destination size 30 bytes
 DCH1CSIZ=30; // 30 bytes transferred per event
-DCH2SSA=&arrC1+0x40000000; // transfer source physical address
-DCH2DSA=&destC+0x40000000; // transfer destination physical address
+DCH2SSA=KVA_TO_PA(&arrC2[0]); // transfer source physical address
+DCH2DSA=KVA_TO_PA(&destC[0]); // transfer destination physical address
 DCH2SSIZ=30; // source size 30 bytes
 DCH2DSIZ=30; // destination size 30 bytes
 DCH2CSIZ=30; // 30 bytes transferred per event
 DCH0INTCLR=0x00ff00ff; // clear existing events, disable all interrupts
-DCH0INTbits.CHSDIE=1;//enable the destination done interrupt
+DCH0INTbits.CHBCIE=1;//enable the destination done interrupt
 DCH1INTCLR=0x00ff00ff; // clear existing events, disable all interrupts
-DCH1INTbits.CHSDIE=1;//enable the destination done interrupt
+DCH1INTbits.CHBCIE=1;//enable the destination done interrupt
 DCH2INTCLR=0x00ff00ff; // clear existing events, disable all interrupts
-DCH2INTbits.CHSDIE=1;//enable the destination done interrupt
+DCH2INTbits.CHBCIE=1;//enable the destination done interrupt
 IPC9CLR=0x0000001f; // clear the DMA channel 0 priority and sub-priority
 IPC9SET=0x00000006; // set IPL 1, sub-priority 2
 IPC9CLR=0x00001F00; // clear the DMA channel 1 priority and sub-priority
@@ -238,16 +185,19 @@ DCH2CONSET=0x80; // turn channel on
 void DMA0_ISR ()
 {
 IFS1CLR=0x00010000; // clear existing DMA channel 0 interrupt flag
+DCH0ECONbits.CABORT=0b1;
 }
 
 void DMA1_ISR ()
 {
 IFS1CLR=0x00020000; // clear existing DMA channel 1 interrupt flag
+DCH1ECONbits.CABORT=0b1;
 }
 
 void DMA2_ISR ()
 {
 IFS1CLR=0x00040000; // clear existing DMA channel 2 interrupt flag
+DCH2ECONbits.CABORT=0b1;
 }
 
 int main(){
@@ -255,7 +205,9 @@ int main(){
 
 	MCU_init();
 	ADCcSonfig();
+DMAconfig();
 	AD1CON1bits.SAMP = 1;
+
 	while(1)
     {
     if(count>=30)
@@ -265,8 +217,14 @@ for(i1=0;i1<=29;i1++)
 {
 arrA2[i1]=arrA1[i1];
 }
-strcpy(arrB2, arrB1);
-strcpy(arrC2, arrC1);
+for(i1=0;i1<=29;i1++)
+{
+arrB2[i1]=arrB1[i1];
+}
+for(i1=0;i1<=29;i1++)
+{
+arrC2[i1]=arrC1[i1];
+}
 count=0;
 DCH0ECONSET=0x00000080;//SET CFORCE to be 1 to start dma transfer
 DCH1ECONSET=0x00000080;//SET CFORCE to be 1 to start dma transfer
